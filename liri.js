e88fb420f29a69ddbaac7d3bf08ff117659d6129
Julia Pauletti <julia.pauletti@yahoo.com>
2019-05-06 20:23:22 -0500

    added log capabilities for each search

--- liri.js
+++ liri.js
@@ -46,10 +46,42 @@ function getConcerts (artistName) {
             if (i !== (response.data.length -1)) {
                 console.log("\n");
             }
+
+            var toLog = {
+                venue: response.data[i].venue.name,
+                location: response.data[i].venue.city + ", " + response.data[i].venue.region,
+                date: date,
+                time: moment(response.data[i].datetime).format("hh:mm A")
+            };
+
+            // also log the data to the txt file
+            fs.appendFileSync("log.txt", JSON.stringify(toLog, null, 2) + "\n\n", function(err) {
+
+                // If an error was experienced we will log it.
+                if (err) {
+                console.log(err);
+                } else {  // if no error is experienced
+                console.log("Search saved to log.txt.");
+                }
+
+            });
+
         }
 
         if (response.data.length === 0) {
             console.log("There are no upcoming events for that artist or band.");
+
+            // if no results, log this
+            fs.appendFileSync("log.txt", "There are no upcoming events for that artist or band." + "\n\n", function (err) {
+
+                // If an error was experienced we will log it.
+                if (err) {
+                    console.log(err);
+                } else {  // if no error is experienced
+                    console.log("Search saved to log.txt.");
+                }
+
+            });
         }
 
 
@@ -67,14 +99,33 @@ function getConcerts (artistName) {
 function getSongInfo (songName) {
 
     spotify.search({ type: 'track', query: songName }, function(err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
 
-    console.log("Song Name: " + data.tracks.items[0].name);
-    console.log("Artist: " + data.tracks.items[0].artists[0].name);
-    console.log("Album: " + data.tracks.items[0].album.name);
-    console.log("Preview: " + data.tracks.items[0].album.external_urls.spotify); // preview link to the song on spotify
+        console.log("Song Name: " + data.tracks.items[0].name);
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("Preview: " + data.tracks.items[0].album.external_urls.spotify); // preview link to the song on spotify
+
+        var toLog = {
+            songName: data.tracks.items[0].name,
+            artist: data.tracks.items[0].artists[0].name,
+            album: data.tracks.items[0].album.name,
+            previewLink: data.tracks.items[0].album.external_urls.spotify
+        };
+
+        // also log the data to the txt file
+        fs.appendFileSync("log.txt", JSON.stringify(toLog, null, 2) + "\n\n", function (err) {
+
+            // If an error was experienced we will log it.
+            if (err) {
+                console.log(err);
+            } else {  // if no error is experienced
+                console.log("Search saved to log.txt.");
+            }
+
+        });
     });
 }
 
@@ -95,6 +146,29 @@ function getMovie (movieName) {
         console.log("Country Produced: " + response.data.Country);
         console.log("IMDb rating: " + response.data.imdbRating);
         console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
+
+        var toLog = {
+            title: response.data.Title,
+            year: response.data.Year,
+            actors: response.data.Actors,
+            plot: response.data.Plot,
+            language: response.data.Language,
+            countryProduced: response.data.Country,
+            imdbRating: response.data.imdbRating,
+            rottenTomatoesRating: response.data.Ratings[1].Value
+        };
+
+        // also log the data to the txt file
+        fs.appendFileSync("log.txt", JSON.stringify(toLog, null, 2) + "\n\n", function (err) {
+
+            // If an error was experienced we will log it.
+            if (err) {
+                console.log(err);
+            } else {  // if no error is experienced
+                console.log("Search saved to log.txt.");
+            }
+
+        });
 
     })
     .catch(function (error) {
@@ -157,8 +231,23 @@ function getRandom () {
 
 
 
+
+
+
 // save command entered
 var command = process.argv[2];
+
+var newEntry = "\n\n\n" + "=====================================================================================================================" + "\n";
+
+// log command and search to a txt file
+fs.appendFileSync("log.txt", newEntry + command + ": " + process.argv.slice(3).join(" ") + "\n\n", function (err) {
+    // If an error was experienced we will log it.
+    if (err) {
+        console.log(err);
+    } else {  // if no error is experienced
+        console.log("Search saved to log.txt.");
+    }
+});
 
 
 // instructions to follow for each command
@@ -211,102 +300,3 @@ switch (command) {
         break;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 9. Make it so liri.js can take in one of the following commands:
-
-// * `concert-this`
-
-// * `spotify-this-song`
-
-// * `movie-this`
-
-// * `do-what-it-says`
-
-// ### What Each Command Should Do
-
-// 1. `node liri.js concert-this <artist/band name here>`
-
-// * This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
-
-//     * Name of the venue
-
-//     * Venue location
-
-//     * Date of the Event (use moment to format this as "MM/DD/YYYY")
-
-// 2. `node liri.js spotify-this-song '<song name here>'`
-
-// * This will show the following information about the song in your terminal/bash window
-
-//     * Artist(s)
-
-//     * The song's name
-
-//     * A preview link of the song from Spotify
-
-//     * The album that the song is from
-
-// * If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-// * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) package in order to retrieve song information from the Spotify API.
-
-// * The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a **client id** and **client secret**:
-
-// * Step One: Visit <https://developer.spotify.com/my-applications/#!/>
-
-// * Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-
-// * Step Three: Once logged in, navigate to <https://developer.spotify.com/my-applications/#!/applications/create> to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-
-// * Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the [node-spotify-api package](https://www.npmjs.com/package/node-spotify-api).
-
-// 3. `node liri.js movie-this '<movie name here>'`
-
-// * This will output the following information to your terminal/bash window:
-
-//     ```
-//     * Title of the movie.
-//     * Year the movie came out.
-//     * IMDB Rating of the movie.
-//     * Rotten Tomatoes Rating of the movie.
-//     * Country where the movie was produced.
-//     * Language of the movie.
-//     * Plot of the movie.
-//     * Actors in the movie.
-//     ```
-
-// * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-
-//     * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
-
-//     * It's on Netflix!
-
-// * You'll use the `axios` package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use `trilogy`.
-
-// 4. `node liri.js do-what-it-says`
-
-// * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-
-//     * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-
-//     * Edit the text in random.txt to test out the feature for movie-this and concert-this.
